33363c5df0b5c4c19cedddb00651be923c0ce828
hujinbin <437044048@qq.com>
2024-06-20 11:11:58 +0800

    feat: 项目初始化

---
+++ main.js
@@ -0,0 +1,56 @@
+const { app, BrowserWindow } = require('electron')
+
+const url = require('url');
+const path = require('path');
+
+function createWindow () {   
+  // 创建浏览器窗口
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  // 加载index.html文件
+  if (process.env.MODE_ENV === 'development') {
+    // 【开发时候使用】
+    //需要和本地项目启动会端口号一致，一般不需要改。多项目启动会有端口被占用而 改变情况
+    win.loadURL('http://localhost:3000/'); 
+  } else {
+    // 【打包时候使用】__dirname为当前文件路径
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, './build/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  }
+//   win.loadFile('index.html')
+
+  // win.loadURL('http://localhost:3000')
+
+   // 解决应用启动白屏问题
+   win.on('ready-to-show', () => {
+    win.show();
+    win.focus();
+  });
+
+  // 当窗口关闭时发出。在你收到这个事件后，你应该删除对窗口的引用，并避免再使用它。
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  });
+  
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  });
